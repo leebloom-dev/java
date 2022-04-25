b98ad5dbefeec1335380d6fa0ac10dc9ca4fe34c
Andrew Lee Bloom <leebloom@Lees-MBP-M1.lan>
2022-04-25 13:30:48 -0400

    gradle

--- src/main/java/exercise/Main.java
+++ src/main/java/exercise/Main.java
@@ -1,4 +1,15 @@
-package src;
+package exercise;
+
+import exercise.casting.Casting;
 
 public class Main {
+
+    public static void main(String[] args) {
+        // Casting
+        System.out.println("crap");
+        Casting casting = new Casting();
+        System.out.println("Casting");
+        casting.display();
+    }
+
 }
